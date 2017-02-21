4831395fc2477b71efa123ae4988e02f22d03efc
Dell <metaslouie038@gmail.com>
2017-02-21 21:16:36 +0800

    Second Activity

---
+++ app/src/main/java/com/example/androidstudiogitproject/NewActivity.java
@@ -0,0 +1,25 @@
+package com.example.androidstudiogitproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dell on 2/21/2017.
+ */
+
+public class NewActivity extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
